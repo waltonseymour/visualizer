f0ba9d6e7b1ab7392ab84b1d2e986e163d28d59d
waltonseymour <walton.l.seymour@gmail.com>
2020-12-28 15:15:46 -0500

    save rust example

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+const runWasm = async () => {
+  // Instantiate our wasm module
+
+  let module = await import("./pkg");
+
+  return module.run();
+};
+
 const App: React.FC = () => {
   const [file, setFile] = useState<File>();
 
   useEffect(() => {
-    const audioCtx = new AudioContext();
-    const audio = new Audio();
+    if (file == null) {
+      return;
+    }
+    const t0 = performance.now();
 
-    let buffer: AudioBuffer;
-    file
-      ?.arrayBuffer()
-      .then((buf) => {
-        return audioCtx.decodeAudioData(buf, (data) => (buffer = data));
-      })
-      .then(() => {
-        const source = audioCtx.createBufferSource();
-        source.buffer = buffer;
-        source.connect(audioCtx.destination);
-        source.start();
-      });
-
-    audio.play();
+    runWasm().then(() => {
+      const t1 = performance.now();
+      console.log(`playing song took ${t1 - t0} milliseconds.`);
+    });
   }, [file]);
 
   return (
@@ -33,8 +33,15 @@ const App: React.FC = () => {
           const file = e.target.files[0];
           setFile(file);
         }}
+        id="file-input"
         type="file"
       />
+      <canvas
+        id="canvas"
+        style={{ height: "600px", width: "800px" }}
+        height={1200}
+        width={1600}
+      ></canvas>
     </div>
   );
 };
